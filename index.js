e9a2d9ca0bc2bf65ff4a3990abeed452a7e09315
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-21 11:59:11 -0400

    Create employee record function

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+const createEmployeeRecord = (employeeArray) => {
+  const [name, lastName, profession, pay] = employeeArray.map((item) => item)
+  const employeeRecord = {}
+
+  employeeRecord.firstName = name
+  employeeRecord.familyName = lastName
+  employeeRecord.title = profession
+  employeeRecord.payPerHour = pay
+  employeeRecord.timeInEvents = []
+  employeeRecord.timeOutEvents = []
+
+  return employeeRecord
+}
+
+const createEmployeeRecords = () => {
+  
+}
